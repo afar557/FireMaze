e8883df4da94fdf6f08f712467b3e4852e344cd6
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-02-03 19:34:04 -0500

    Removed commented code

--- bfs.py
+++ bfs.py
@@ -4,36 +4,6 @@ maze = [[0,0,0],
         [0,0,0],
         [0,0,0]]
 
-# def bfs(maze, start, finish):
-#     queue = deque()
-#     queue.append([[start]])
-
-#     visited = [[0 for y in range(len(maze))] for x in range(len(maze[0]))]
-#     # print(visited)
-#     row = 0
-#     col = 0
-#     while queue:
-#         curr = queue.popleft()
-#         row, col = curr[-1]
-#         if row < 0 or col < 0 or row >= len(maze) or col >= len(maze[0]) or visited[row][col] == 1 or maze[row][col] == 1:
-#             continue
-        
-#         visited[row][col] = 1
-#         # maze[row][col] = 2
-#         print('x: ',row,'y:',col)
-#         queue.append(curr + [(row+1,col)]) # move down
-#         queue.append(curr + [(row-1,col)]) # move up
-#         queue.append(curr + [(row,col+1)]) # move right
-#         queue.append(curr + [(row,col-1)]) # move left
-#         # print('x: ',row,'y:',col)
-
-#         if row == finish[0] and col == finish[1]:
-#             print('reached goal')
-#             return maze
-        
-#     print('did not reach goal')
-#     return maze
-
 def bfs(maze, start, finish):
     width = len(maze[0])
     height = len(maze)
