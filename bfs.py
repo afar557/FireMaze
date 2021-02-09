53a9dc0eeded3fc79f8728d33c3d30a43953e171
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-02-09 15:49:44 -0500

    Fixed bfs to include blocked spots

--- bfs.py
+++ bfs.py
@@ -1,4 +1,3 @@
-# ADD CHECKS TO SEE IF GOING TO BLOCKED POSITION
 from collections import deque
 
 maze = [[0,0,0],
@@ -18,7 +17,7 @@ def bfs(maze, start, finish):
                 maze[position[0]][position[1]] = 2 # mark the shortest path to finish in the maze as 2
             return maze
         for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)): # explore all neighbors up, down, right, left
-            if 0 <= x2 < width and 0 <= y2 < height and (x2, y2) not in seen: # check if neighbors are in bounds and not visited
+            if 0 <= x2 < width and 0 <= y2 < height and (x2, y2) not in seen and maze[x2][y2] == 0: # check if neighbors are in bounds and not visited
                 queue.append(path + [(x2, y2)]) # add neighbors and its path to the fringe
                 seen.add((x2, y2))
     return maze
