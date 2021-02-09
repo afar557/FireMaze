2a4b4e2bc417486216f56a11bcbe9b108e7d0555
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-02-09 15:48:57 -0500

    Strategy 1 using bfs

---
+++ strategy1.py
@@ -0,0 +1,32 @@
+from collections import deque
+from advancefire import advance_fire_one_step
+import random
+
+maze = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+def bfsStrat1(maze, start, finish, q):
+    width = len(maze[0])
+    height = len(maze)
+    queue = deque([[start]])
+    seen = set([start])
+    while queue:
+        path = queue.popleft()
+        x, y = path[-1]
+        if x == finish[0] and y == finish[1]:
+            for position in path:
+                maze = advance_fire_one_step(maze, q)
+                if maze[position[0]][position[1]] == 5:
+                    print("Ran into fire oops :P")
+                    return maze
+                maze[position[0]][position[1]] = 2 # mark the shortest path to finish in the maze as 2
+            return maze
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)): # explore all neighbors up, down, right, left
+            if 0 <= x2 < width and 0 <= y2 < height and (x2, y2) not in seen and maze[x2][y2] != 1: # check if neighbors are in bounds and not visited and not blocked                
+                queue.append(path + [(x2, y2)]) # add neighbors and its path to the fringe
+                seen.add((x2, y2))
+    print("Goal unreachable")
+    return maze
+
+# bfs(maze, (0,0), (2,2))
