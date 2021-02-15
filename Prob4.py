55d7a9b5ab8dcaffa9ce83873d604f1373935a42
nadaelshamaa <nada.elshamaa@rutgers.edu>
2021-02-15 17:35:55 -0500

    Finished the code for dfs, bfs and A* for prob 4

--- Prob4.py
+++ Prob4.py
@@ -1,13 +1,15 @@
 from time import time
 from generateMaze import generateMaze, generateFireMaze
 from dfs import dfs
+from bfs import bfs
+from aStar import aStar
 
 # FOR PROBLEM 4
-def prob4():
+def prob4dfs():
     p = 0.3
     diff = 0
-    dimension = 5000
-    print("starting")
+    dimension = 4000
+    print("starting DFS")
     while diff<60:
         dimension+=100
         grid = generateMaze(dimension,p)
@@ -17,8 +19,43 @@ def prob4():
 
         diff = end-start
         print(diff , "for dim --> " , dimension)
-        
 
     print(dimension)
     return
-prob4()
+
+def prob4bfs():
+    p = 0.3
+    diff = 0
+    dimension = 5000
+    print("starting BFS")
+    while diff<60:
+        dimension+=100
+        grid = generateMaze(dimension,p)
+        start = time()
+        grid = bfs(grid, (0,0), (dimension-1,dimension-1))
+        end = time()
+
+        diff = end-start
+        print(diff , "for dim --> " , dimension)
+        
+    print(dimension)
+    return
+
+def prob4Astar():
+    p = 0.3
+    diff = 0
+    dimension = 5000
+    print("starting A*")
+    while diff<60:
+        dimension+=100
+        grid = generateMaze(dimension,p)
+        start = time()
+        grid = aStar(grid, (0,0), (dimension-1,dimension-1))
+        end = time()
+
+        diff = end-start
+        print(diff , "for dim --> " , dimension)
+        
+    print(dimension)
+    return
+prob4dfs()
---
+++ Prob6Strt1Plot.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from strategy1 import bfsStrat1
+from generateMaze import generateMaze, generateFireMaze
