bf8efb9972e6a6ca5c0d4bfb29fc7f86dbd988bb
nadaelshamaa <nada.elshamaa@rutgers.edu>
2021-02-15 17:08:27 -0500

    Commented out code for problem 4

--- main.py
+++ main.py
@@ -26,24 +26,24 @@ def main():
     # grid = bfsStrat1(grid, start, finish, q)
     # grid = bfsStrat2(grid, start, finish, q)
 
-    # FOR PROBLEM 4
-    p = 0.3
-    diff = 0
-    dimension = 5000
-    print("starting")
-    while diff<60:
-        dimension+=100
-        grid = generateMaze(dimension,p)
-        start = time()
-        grid = dfs(grid, (0,0), (dimension-1,dimension-1))
-        end = time()
+    # # FOR PROBLEM 4
+    # p = 0.3
+    # diff = 0
+    # dimension = 5000
+    # print("starting")
+    # while diff<60:
+    #     dimension+=100
+    #     grid = generateMaze(dimension,p)
+    #     start = time()
+    #     grid = dfs(grid, (0,0), (dimension-1,dimension-1))
+    #     end = time()
 
-        diff = end-start
-        print(diff , "for dim --> " , dimension)
+    #     diff = end-start
+    #     print(diff , "for dim --> " , dimension)
         
 
-    print(dimension)
-    return
+    # print(dimension)
+    # return
 
 
     # Define colors for maze
