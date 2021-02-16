e3ebcbbddc852d2fd1c82528eeb819209ebe7d4a
Aamna Farooq <aaf.zakaria@gmail.com>
2021-02-16 03:19:13 -0500

    for loop w decial range for obstacleDensity & to calculate num of nodes visited

--- Problem3/generatePlot.py
+++ Problem3/generatePlot.py
@@ -7,3 +7,19 @@ from generateMaze import generateMaze
 from Problem3.bfs import bfs
 from Problem3.aStar import aStar
 from dfs import dfs
+
+    dimension = 100
+    start = (0,0)
+    finish = (dimension-1,dimension-1)
+
+    bfsNodes =[]
+    aStarNodes = []
+    obstacleDensity = []
+
+    for x in np.arange(0, 1, 0.001):
+        p = x.item()
+        obstacleDensity.append(p)
+        bfsSum=0
+        aStarSum=0
+
+        grid = generateMaze(dimension,p)
