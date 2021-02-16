d8d132417b8489a4f82c9fab9348c6299658903a
Aamna Farooq <aaf.zakaria@gmail.com>
2021-02-16 03:17:15 -0500

    handle paths for folder

---
+++ Problem3/generatePlot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys, os
+sys.path.append("..")
+
+from generateMaze import generateMaze
+from Problem3.bfs import bfs
+from Problem3.aStar import aStar
+from dfs import dfs
