cf2b1590e3eebea85660a0d2630a6bddffb67e4e
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-02-17 18:24:26 -0500

    Correct version of code

--- strategy3.py
+++ strategy3.py
@@ -21,54 +21,23 @@ def heuristic(a,b):
             
 # Function that executes the A* algorithm and returns the maze with the path
 def aStarF(maze, start, finish, q):
-    global prev
-    global fringe
-    global currentDistance
+    # Create fringe using a priority queue
+    fringe = PriorityQueue()
 
+    # Add in the starting index (0,0) to the queue
+    fringe.put((0, start))
+
+    # Create a dictionary to store previously visited indices that you can use to trace a path
+    prev= {}
+    prev[start] = None
+
+    # Create a dictionary to store the current distance traveresed
+    currentDistance = {}
+    currentDistance[start] = 0
 
     # Initialize x,y values to start
     x,y = start
-    current = start
-    
-    # Explore all 4 neighbors of the current cell: up, down, right and left
-    for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-        # If the coordinates are in bounds and the neighbor cell is open
-        if 0 <= x2 < len(maze[0]) and 0 <= y2 < len(maze) and maze[x2][y2]==0:
-            # Calculate the new distance from current cell to neighbor by adding 1
-            newDistance = currentDistance[current] + 1
-
-            # If the neighbor has not been previously visited or the new distance to the neighbor is less than the currently stored distance to the neighbor
-            if (x2,y2) not in currentDistance or newDistance < currentDistance[(x2,y2)]:
-                # Update the current distance for the current neighbor
-                currentDistance[(x2,y2)] = newDistance
-
-                # Calculate the priority of the neighbor using the heuristic
-                priority = newDistance + heuristic(finish,(x2,y2))
-
-                fireProb = fireHeuristic(maze, q, (x2,y2))
-
-
-                # Add the neighbor into the queue
-                fringe.put(((fireProb, priority), (x2,y2)))
-
-                # Add the neighbor into prev
-                prev[(x2,y2)] = current
-
-
-def driver(maze, start, finish, q):
-    global prev
-    global fringe
-    global currentDistance
-    
-    currentDistance[start] = 0
-
-    prev[start] = None
-
-    # Add in the starting index (0,0) to the queue
-    # tuple[0]= fireHeurisitc, tuple[1] = euclidean distance
-    fringe.put(((0,0), start))
     count=0
-    current = start
     while not fringe.empty():
         # Get the first index in the queue
         current = fringe.get()[1]
